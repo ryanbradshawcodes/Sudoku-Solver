483586c045f7741319b34568f579868fee979e1d
ryanbradshawcodes <ryan.bradshaw@talktalk.net>
2022-08-11 16:01:53 +0100

    Seg fault in boardIsValid

--- solver2.c
+++ solver2.c
@@ -11,20 +11,9 @@ Output: Solutions to the Sudoku problems
 #include <math.h>
 #include <ctype.h>
 
-#define SIZE 4
+#define SIZE 9
 
-// Create a 2D array to store the Sudoku board
 int board[SIZE][SIZE];
-
-// Structure to store data for each board cell
-struct cell {
-    int value;
-    int possibleValues[SIZE];
-    int row;
-    int col;
-};
-
-struct cell cells[SIZE][SIZE];
 
 // Reads file and gets the Sudoku board
 void importBoard() {
@@ -32,8 +21,7 @@ void importBoard() {
     FILE *file;
     file = fopen("sudoku.txt", "r");
     if (file == NULL) {
-        // Create a new file if it doesn't exist
-        // Of a grid consisting of 0s
+        // Create a new file with grid of 0s
         file = fopen("sudoku.txt", "w");
         for (i = 0; i < SIZE; i++) {
             for (j = 0; j < SIZE; j++) {
@@ -44,77 +32,110 @@ void importBoard() {
         fclose(file);
     }
 
+    // Read the file and store the board in a 2D array
+    file = fopen("sudoku.txt", "r");
     for (i = 0; i < SIZE; i++) {
         for (j = 0; j < SIZE; j++) {
-            cells[i][j].row = i;
-            cells[i][j].col = j;
-
-            // TODO Allow sudoku.txt to be formatted and get read properly
-
-            if (fscanf(file, "%d", &cells[i][j].value) == 1) {
-                // If the cell has a value, set the possible values to 0
-                if (cells[i][j].value != 0) {
-                    for (int k = 0; k < SIZE; k++) {
-                        cells[i][j].possibleValues[k] = 0;
-                    }
-                }
-                // Otherwise, set the possible values to 1-SIZE
-                else {
-                    for (int k = 0; k < SIZE; k++) {
-                        cells[i][j].possibleValues[k] = k + 1;
-                    }
-                }
-            }
+            fscanf(file, "%d", &board[i][j]);
         }
     }
+
     fclose(file);
 }
 
 // Prints the Sudoku board
 void printBoard() {
     int i, j;
-    printf("\n");
     for (i = 0; i < SIZE; i++) {
         if (i % (int) sqrt(SIZE) == 0) {
-            printf("+-----+-----+\n");
+            printf("+-------+-------+-------+\n");
         }
         for (j = 0; j < SIZE; j++) {
             if (j % (int) sqrt(SIZE) == 0) {
                 printf("| ");
             }
-            printf("%d ", cells[i][j].value);
+            printf("%d ", board[i][j]);
         }
         printf("|\n");
     }
-    printf("+-----+-----+\n\n");
+    printf("+-------+-------+-------+\n\n");
 }
 
 // 1 for valid, 0 for invalid
 int boardIsValid() {
     // Every number in each sub-grid must be unique
     int i, j, k, l;
-    for (i = 0; i < (int) sqrt(SIZE); i++) {
-        for (j = 0; j < (int) sqrt(SIZE); j++) {
-            for (k = 0; k < (int) sqrt(SIZE); k++) {
-                for (l = 0; l < (int) sqrt(SIZE); l++) {
-                    if (cells[i][j].value == cells[k][l].value && cells[i][j].value != 0 && i != k && j != l) {
+    for (i = 0; i < SIZE; i++) {
+        for (j = 0; j < SIZE; j++) {
+            if (board[i][j] != 0) {
+                for (k = 0; k < SIZE; k++) {
+                    if (board[i][k] == board[i][j] && k != j) {
+                        return 0;
+                    }
+                }
+                for (l = 0; l < SIZE; l++) {
+                    if (board[l][j] == board[i][j] && l != i) {
                         return 0;
                     }
                 }
             }
         }
     }
-    int i2, j2;
+
+    // Every number in each row must be unique
+    int i2, j2, k2;
     for (i2 = 0; i2 < SIZE; i2++) {
         for (j2 = 0; j2 < SIZE; j2++) {
-            int k2;
-            for (k2 = 0; k2 < SIZE; k2++) {
-                if (cells[i2][j2].value == cells[i2][k2].value && j2 != k2 && cells[i2][j2].value != 0) {
-                    return 0;
+            if (board[i2][j2] != 0) {
+                for (k2 = 0; k2 < SIZE; k2++) {
+                    if (board[i2][k2] == board[i2][j2] && k2 != j2) {
+                        return 0;
+                    }
                 }
-                if (cells[i2][j2].value == cells[k2][j2].value && i2 != k2 && cells[i2][j2].value != 0) {
-                    return 0;
+            }
+        }
+    }
+
+    // Every number in each column must be unique
+    int i3, j3, k3;
+    for (i3 = 0; i3 < SIZE; i3++) {
+        for (j3 = 0; j3 < SIZE; j3++) {
+            if (board[j3][i3] != 0) {
+                for (k3 = 0; k3 < SIZE; k3++) {
+                    if (board[k3][i3] == board[j3][i3] && k3 != j3) {
+                        return 0;
+                    }
                 }
+            }
+        }
+    }
+
+    return 1;
+}
+
+/**
+ * @brief Solves the Sudoku board using backtracking algorithm
+ * @param board The current state of the Sudoku board
+ * @return int 1 for solved, 0 for not solved
+ */
+int backtrack(int board[SIZE][SIZE]) {
+    int i, j;
+    for (i = 0; i < SIZE; i++) {
+        for (j = 0; j < SIZE; j++) {
+            if (board[i][j] == 0) {
+                int k;
+                for (k = 1; k <= SIZE; k++) {
+                    board[i][j] = k;
+                    if (!boardIsValid()) {
+                        board[i][j] = 0;
+                    }
+                    if (backtrack(board)) {
+                        return 1;
+                    } else {
+                        board[i][j] = 0;   
+                    }
+                }
+                return 0;
             }
         }
     }
@@ -122,10 +143,15 @@ int boardIsValid() {
 }
 
 void solveBoard() {
-
+    if (backtrack(board)) {
+        printBoard();
+    } else {
+        printf("No solution exists\n");
+    }
 }
 
 int main() {
+    printf("\n");
     importBoard();
     if (!boardIsValid()) {
         printf("The board is invalid. Please import another board.\n\n");
@@ -146,7 +172,7 @@ int main() {
             solveBoard();
             printBoard();
 
-        } else if (choice == 2){
+        } else if (choice == 2) {
             // Create a new board
             importBoard();
             if (!boardIsValid()) {
